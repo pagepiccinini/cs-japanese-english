aee9bf9efd835a72fc46408a9074fe888572ee38
Page Piccinini <page.piccinini@gmail.com>
2017-03-10 17:21:25 +0100

    Add new textgrids and textfiles. Update discourse marker script for Japanese.

---
+++ scripts/analyses/cs_japanese_english_analysis_discmark.R
@@ -0,0 +1,19 @@
+## Notes, look at [n] durations and vowel quality for "nanka"
+
+## ORGANIZE DATA ####
+# Focus on "like"s for English
+data_eng_like = data_eng_clean %>%
+  # Only look at tokens of the word like
+  filter(word == "like")
+
+# Focus on "nanka"s for Japanese
+data_jap_nanka = data_jap_clean %>%
+  # Only look at tokens of the word nanka
+  filter(word == "ナンカ")
+
+# Get position counts
+data_eng_like_sum = data_eng_like %>%
+  count(word_position)
+  
+data_jap_nanka_sum = data_jap_nanka %>%
+  count(word_position)

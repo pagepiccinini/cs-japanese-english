c8ba062db2c1d07a897bd0a00cfd1e762bc9ce2d
Page Piccinini <page.piccinini@gmail.com>
2017-07-24 21:22:57 -0700

    Refractor words analysis.

---
+++ word_analysis/scripts/words_packages.R
@@ -0,0 +1,15 @@
+# General data manipulation and plotting
+library(tidyverse)
+
+# Manipulation in Praat
+library(PraatR)
+
+# Text analysis
+library(tidytext)
+
+# Sound manipulation
+library(seewave)
+library(tuneR)
+
+# Specific color choices
+library(RColorBrewer)

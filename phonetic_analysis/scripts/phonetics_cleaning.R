824736a312fe6df84f18799976ab687d3cfa92f2
Page Piccinini <page.piccinini@gmail.com>
2018-01-07 14:27:51 -0800

    Add demographic information to cleaning script.

--- phonetic_analysis/scripts/phonetics_cleaning.R
+++ phonetic_analysis/scripts/phonetics_cleaning.R
@@ -1,17 +1,22 @@
-## READ IN WORD COUNT DATA
-data_word_count = read.table("word_analysis/data/generated/wordcounts.txt",header=T)
+## READ IN WORD COUNT DATA ####
+data_word_count = read.table("word_analysis/data/generated/wordcounts.txt",header=T) %>%
+  # Add in demographic information
+  inner_join(demo_simple)
+
 
 ## READ IN DATA FOR DURATION ####
 # Read in data
 data_duration = list.files("phonetic_analysis/data",
                        pattern = "*_duration.csv", full.names = T) %>%
   map(read_csv) %>%
-  bind_rows()
+  bind_rows() %>%
+  # Add in demographic information
+  inner_join(demo_simple)
 
 # Clean data
 data_duration_clean = data_duration %>%
   mutate(duration_ms = if_else(presence == 0, 0, duration_ms)) %>%
-  left_join(data_word_count,by = c("prompt","pair","speaker"))
+  left_join(data_word_count, by = c("prompt","pair","speaker", "acquisition_English_pct_exposed"))
 
 
 ## READ IN DATA FOR FORMANTS ####
@@ -19,7 +24,9 @@ data_duration_clean = data_duration %>%
 data_formants = list.files("phonetic_analysis/data",
                            pattern = "*_formants.csv", full.names = T) %>%
   map(read_csv) %>%
-  bind_rows()
+  bind_rows() %>%
+  # Add in demographic information
+  inner_join(demo_simple)
 
 # Clean data
 data_formants_clean = data_formants %>%
@@ -30,6 +37,6 @@ data_formants_clean = data_formants %>%
   # Normalize F1 and F2 based on F3
   mutate(f1_norm_bark = f3_bark - f1_bark) %>%
   mutate(f2_norm_bark = f3_bark - f2_bark) %>%
-  left_join(data_word_count,by = c("prompt","pair","speaker"))
+  left_join(data_word_count, by = c("prompt","pair","speaker", "acquisition_English_pct_exposed"))
 
 
